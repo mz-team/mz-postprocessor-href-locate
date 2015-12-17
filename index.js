9c1173afb9825cb6410ecb697e0f92e241a5745e
yakey <414739025@qq.com>
2015-12-17 20:33:12 +0800

    commit

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ module.exports = function(content, file, setting) {
         if(value.substr(0,1) === '/' && value.indexOf(setting.left_delimiter) == -1){
 
             var f = fis.uri(value);
-            
 
             if(f.file) {
-                all = prefix + 'href=' + quote + f.file.release + query + quote + postfix
+                if(f.file.useHash){
+                    var releaseFile = f.file.release, 
+                        realName = releaseFile.substring(0, releaseFile.lastIndexOf('.')), 
+                        extName = releaseFile.substr(releaseFile.lastIndexOf('.') + 1);
+
+                    all = prefix + 'href=' + quote + f.file.domain + realName + '_' + f.file.getHash() + '.' + extName + query + quote + postfix;
+                }else{
+                    all = prefix + 'href=' + quote + f.file.domain + f.file.release + query + quote + postfix;
+                }
             }           
         }
 
